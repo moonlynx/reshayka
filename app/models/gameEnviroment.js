509afc8d667614f32ffaf22eaa60a96f791638e9
moonlynx <moonlynx@yandex.ru>
2017-12-20 13:13:58 +0300

    indentation has been updated

--- app/models/gameEnviroment.js
+++ app/models/gameEnviroment.js
@@ -1,11 +1,11 @@
 define(["models/mathgen-amd"], 
 
 function(mathgen){
-    return {
-        correctAnswers: 0,
-        incorrectAnswers: 0,
-        motiv: "",
-        example: mathgen.getExample("+", 10),
-        answer: ""
-    };
+  return {
+    correctAnswers: 0,
+    incorrectAnswers: 0,
+    motiv: "",
+    example: mathgen.getExample("+", 10),
+    answer: ""
+  };
 });
