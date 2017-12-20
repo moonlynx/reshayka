c42b7f7b8e01f4bc06f3b4463204bab01e52a389
moonlynx <moonlynx@yandex.ru>
2017-12-20 10:12:52 +0300

    add settings config

---
+++ app/models/settings.js
@@ -0,0 +1,11 @@
+define(function() {
+    return {
+        operators: ['+'],
+        maxExampleNumber: 10,
+        maxNumberForAll: false,
+        addMaxNumber: 10,
+        subMaxNumber: 10,
+        mulMaxNumber: 10,
+        divMaxNumber: 10,
+    }
+});
