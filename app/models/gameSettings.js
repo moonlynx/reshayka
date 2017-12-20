d30ab785a0aab537e4e29ef0a9177cee67d1c8b3
moonlynx <moonlynx@yandex.ru>
2017-12-20 13:45:05 +0300

    general beauty guidance

--- app/models/gameSettings.js
+++ app/models/gameSettings.js
@@ -1,11 +1,9 @@
-define(function() {
-  return {
-    operators: ['+'],
-    maxExampleNumber: 10,
-    maxNumberForAll: false,
-    addMaxNumber: 10,
-    subMaxNumber: 10,
-    mulMaxNumber: 10,
-    divMaxNumber: 10
-  }
+define({
+  operators: ['+'],
+  maxExampleNumber: 10,
+  maxNumberForAll: false,
+  addMaxNumber: 10,
+  subMaxNumber: 10,
+  mulMaxNumber: 10,
+  divMaxNumber: 10
 });
